5530a46fa71614df9c7b94086a28d0b05f88c89c
Maksim Alekseev <mrvalter566@gmail.com>
2023-11-05 19:00:58 +0700

    Initial setup for spotify sdk

---
+++ iPodSkin-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  iPodSkin-Bridging-Header.h
+//  iPodSkin
+//
+//  Created by Максим Алексеев  on 05.11.2023.
+//
+
+#ifndef iPodSkin_Bridging_Header_h
+#define iPodSkin_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+
+#endif /* iPodSkin_Bridging_Header_h */
--- iPodSkin/iPodSkinApp.swift
+++ iPodSkin/iPodSkinApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct iPodSkinApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
