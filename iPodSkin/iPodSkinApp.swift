46739a37ce213d5254f2d73af8172e70b2ce37d9
Maksim Alekseev <mrvalter566@gmail.com>
2023-11-05 13:58:33 +0700

    Initial commit

---
+++ iPodSkin/iPodSkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPodSkinApp.swift
+//  iPodSkin
+//
+//  Created by Максим Алексеев  on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iPodSkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
